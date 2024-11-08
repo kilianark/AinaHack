5701ae12a86c4adb53941d7bc62436e0516c147d
Kilian Morillas <kilian08momo@gmail.com>
2024-11-08 20:49:54 +0100

    header buttons opperative

--- angular-front/src/app/app-routing.module.ts
+++ angular-front/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { TraductorTextComponent } from "./pages/traductor-text/traductor-text.component";
+import { TraductorDocComponent } from "./pages/traductor-doc/traductor-doc.component";
 
 export const routes: Routes = [
     {path: 'translate_text', component: TraductorTextComponent},
+    {path: 'translate_pdf', component: TraductorDocComponent},
     {path: '', redirectTo: '/translate_text', pathMatch: 'full'},
     {path: '**', redirectTo: '/translate_text', pathMatch: 'full'},
 ];
