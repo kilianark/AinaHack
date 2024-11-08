df333ce575de6252e80d00f1ce42c5d248093b23
KumiGaooITICBCN <2023_moises.diez@iticbcn.cat>
2024-11-08 18:48:32 +0100

    salamandra y main

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,4 +1,16 @@
-from ainaTransCatToEs import translate_text
+from salamandra import translate_text
 
-input_text =input("Introduce la frase que quieras traducir: ")
-print( translate_text(input_text))  
+def main():
+    # Example sentence to translate
+    src_lang_code = 'Catalan'
+    tgt_lang_code = 'Spanish'
+    sentence = input("Introduzca el la frase que quiere traducir: ")
+    # Call the translate_text function
+    translated_text = translate_text(src_lang_code, tgt_lang_code, sentence)
+
+    # Print the result
+    print("Original:", sentence)
+    print("Translated:", translated_text)
+
+if __name__ == "__main__":
+    main()
