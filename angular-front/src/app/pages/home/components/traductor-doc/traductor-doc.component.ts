0587438ed8c9f6f17175b2796d619007ec58ed6f
Helena <helenagp05@gmail.com>
2024-11-08 18:51:58 +0100

    Creacion de componentes

---
+++ angular-front/src/app/pages/home/components/traductor-doc/traductor-doc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traductor-doc',
+  standalone: true,
+  imports: [],
+  templateUrl: './traductor-doc.component.html',
+  styleUrl: './traductor-doc.component.css'
+})
+export class TraductorDocComponent {
+
+}
