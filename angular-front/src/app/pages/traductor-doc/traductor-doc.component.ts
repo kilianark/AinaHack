3bd5c660312248d13a1cd8494eea4aa7b597ebc9
Helena <helenagp05@gmail.com>
2024-11-08 22:33:44 +0100

    Input y output de traducir

--- angular-front/src/app/pages/traductor-doc/traductor-doc.component.ts
+++ angular-front/src/app/pages/traductor-doc/traductor-doc.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-traductor-doc',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule],
   templateUrl: './traductor-doc.component.html',
   styleUrl: './traductor-doc.component.css'
 })
 export class TraductorDocComponent {
+  traductorForm: FormGroup;
+  submittedText: string | null = null;
 
+  constructor(private fb: FormBuilder){
+    this.traductorForm = this.fb.group({
+      text:['', [Validators.required]]
+    });
+  }
+
+  guardar(): void {
+    if (this.traductorForm.valid) {
+      this.submittedText = this.traductorForm.get('text')?.value;
+    }
+  }
 }
--- angular-front/src/app/pages/traductor-text/traductor-text.component.ts
+++ angular-front/src/app/pages/traductor-text/traductor-text.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
@@ -10,10 +10,25 @@ import { MatInputModule } from '@angular/material/input';
   imports: [CommonModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule  ],
+    FormsModule,
+    ReactiveFormsModule],
   templateUrl: './traductor-text.component.html',
   styleUrl: './traductor-text.component.css'
 })
 export class TraductorTextComponent {
-  text: string = '';
+  traductorForm: FormGroup;
+  submittedText: string | null = null;
+
+  constructor(private fb: FormBuilder){
+    this.traductorForm = this.fb.group({
+      text:['', [Validators.required]]
+    });
+  }
+
+  guardar(): void {
+    if (this.traductorForm.valid) {
+      this.submittedText = this.traductorForm.get('text')?.value;
+    }
+  }
+
 }
