7e5bc30bcc1c5481e39fb9d3271022f4c81d8777
KumiGaoo <119248514+Kumigaoo@users.noreply.github.com>
2024-11-09 03:05:21 +0100

    main truco

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,16 +1,23 @@
 from salamandra import translate_text
-
+from simplify import simplify
 def main():
     # Example sentence to translate
+
+    sentence = input("Introduzca la frase que quiere traducir: ")
+    
     src_lang_code = 'Spanish'
     tgt_lang_code = 'English'
-    sentence = input("Introduzca la frase que quiere traducir: ")
-    # Call the translate_text function
-    translated_text = translate_text(src_lang_code, tgt_lang_code, sentence)
-
-    # Print the result
+    
+    resumed = simplify(sentence)
+    
+    src_lang_code = 'English'
+    tgt_lang_code = 'Catalan'
+    final_text = translate_text(src_lang_code, tgt_lang_code, resumed)
+    
+    
     print("Original:", sentence)
-    print("Translated:", translated_text)
+    print("Translated:", final_text)
+
 
 if __name__ == "__main__":
     main()
