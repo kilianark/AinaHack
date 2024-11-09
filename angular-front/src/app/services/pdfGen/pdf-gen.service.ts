55e35b1567c70db0fbd3ff62fcfd0d3387238550
Kilian Morillas <kilian08momo@gmail.com>
2024-11-09 10:45:05 +0100

    ya nos genera el pdf

---
+++ angular-front/src/app/services/pdfGen/pdf-gen.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import jsPDF from 'jspdf';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PdfGenService {
+
+  constructor() { }
+
+  generatePDF(text: string) {
+    const doc = new jsPDF();
+
+    doc.setFontSize(28);
+    doc.text('Bug Busters', 20, 25);
+
+    //linea divisoria
+    doc.setLineWidth(0.5);
+    doc.line(10, 45, 200, 45);
+
+    doc.setFontSize(12);
+    doc.text(text, 10, 60, { maxWidth: 190 });
+
+    doc.output('dataurlnewwindow');
+  }
+}
